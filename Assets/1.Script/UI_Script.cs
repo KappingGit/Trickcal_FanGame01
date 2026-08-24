96710d921f286756947690fb71353d412cff6f8e
KappingGit <130461538+KappingGit@users.noreply.github.com>
2026-08-24 22:46:02 +0900

    추가 작업(날짜 확인) : 배경음악 사운드 조절
    8월 24일 작업 연장선
    
    1. 옵션 UI 제작(일단 배경 음악 조절을 위해 만들어둔 것)
    
    2. 배경음악 사운드 조절 시스템 구현

--- Assets/1.Script/UI_Script.cs
+++ Assets/1.Script/UI_Script.cs
@@ -9,11 +9,23 @@ public class UI_Script : MonoBehaviour
 {
     [SerializeField] private GameObject PauseUI_Obj;
 
+    [SerializeField] private GameObject OptionUI_Obj;
+
     private bool isPaused = false; // 일시정지 중이라면
+
+    private bool isOption = false;
 
     private void Awake()
     {
         PauseUI_Obj.SetActive(false);
+        OptionUI_Obj.SetActive(false);
+
+        //배경 사운드 관련
+        // 게임 시작 시, 현재 BGM의 실제 볼륨 수치(0.0 ~ 1.0)를 슬라이더 위치에 똑같이 맞춘다
+        if (bgmAudioSource != null && bgmSlider != null)
+        {
+            bgmSlider.value = bgmAudioSource.volume;
+        }
     }
 
     private void Update()
@@ -23,6 +35,12 @@ public class UI_Script : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
+
+            if (isOption)
+            {
+                OptionUI_Obj.SetActive(false);
+                isOption = false;
+            }
         }
 
         if (Time.timeScale == 0f) return; // 이거의 위치에 따라 esc기능이 조금 달라짐
@@ -55,4 +73,37 @@ public class UI_Script : MonoBehaviour
         // "MainMenu"라는 이름의 씬으로 이동 (실제 씬 이름에 맞게 수정)
         SceneManager.LoadScene("MainScene");
     }
+
+    //사운드 옵션 전체 창
+    public void ToggleSoundOption()
+    {
+        if (!isOption) // 사운드 옵션창이 꺼져있고 옵션창이 켜져있다면
+        {
+            OptionUI_Obj.SetActive(true);
+            isOption = true;
+        }
+        else
+        {
+            OptionUI_Obj.SetActive(false);
+            isOption = false;
+        }
+
+    }
+
+    //나중에 사운드 전용 스크립트로 옮길수 있으니 여기로 배치
+    [Header("연결 요소")]
+    [SerializeField] private AudioSource bgmAudioSource; // BGM 스피커
+    [SerializeField] private Slider bgmSlider;          // 볼륨 조절 슬라이더 UI
+
+    // 슬라이더가 움직일 때 실시간으로 호출될 볼륨 조절 함수
+    // 매개변수로 float volume을 받는 것이 핵심
+    //배경화면 사운드 조절(나중에 따로 스크립트 분리할것인지 고려해둘 것)
+    public void BGM_Sound(float volume)
+    {
+        if (bgmAudioSource != null)
+        {
+            bgmAudioSource.volume = volume; // 0.0 ~ 1.0 조절
+        }
+    }
+    
 }
