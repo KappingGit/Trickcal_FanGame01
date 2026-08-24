3efab49b869e1c03df52df122ac65748bebb32a8
KappingGit <130461538+KappingGit@users.noreply.github.com>
2026-08-24 16:04:53 +0900

    한글폰트 적용, 일시정지 및 메인화면 기능
    [주의 사항]
    작업 날짜: 8월 22일(토)
    푸시 날짜: 8월 24일(월)
    
    1. 한글 폰트 적용
    
    2. 일시정지UI 및 일시정지 기능과 메인 메뉴로 돌아가는 버튼을 구현

--- Assets/1.Script/UI_Script.cs
+++ Assets/1.Script/UI_Script.cs
@@ -3,8 +3,55 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro; // 텍스트 메쉬 프로를 사용하기 위해 필요하다.
+using UnityEngine.SceneManagement; // 씬 이동을 위해 추가!
 
 public class UI_Script : MonoBehaviour
 {
-   
+    [SerializeField] private GameObject PauseUI_Obj;
+
+    private bool isPaused = false; // 일시정지 중이라면
+
+    private void Awake()
+    {
+        PauseUI_Obj.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 0f) return;
+
+        // ESC 키를 누르면 일시정지 상태를 전환한다.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    
+    public void TogglePause()
+    {
+        if (!isPaused)
+        {
+            //일시정지
+            PauseUI_Obj.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            PauseUI_Obj.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1f;
+        }
+    }
+
+    // '메인 메뉴로 나가기' 버튼에 연결할 함수
+    public void GoToMainMenu()
+    {
+        // 중요: 씬을 넘어가기 전에 반드시 시간을 정상화해야 함!
+        Time.timeScale = 1f;
+
+        // "MainMenu"라는 이름의 씬으로 이동 (실제 씬 이름에 맞게 수정)
+        SceneManager.LoadScene("MainScene");
+    }
 }
